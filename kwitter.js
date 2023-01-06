baa9f9924a835667275c75e235ccb4d32c72ae03
MOMORIRIDALMEIDA <101433169+MOMORIRIDALMEIDA@users.noreply.github.com>
2023-01-06 17:53:56 -0500

    Update kwitter.js

--- kwitter.js
+++ kwitter.js
@@ -1,6 +1,6 @@
 function addUser() {
 
-    user_name = document.getEleementById("user_name").value;
+    user_name = document.getElementById("user_name").value;
     localStorage.setItem("user_name", user_name);
     window.location = "kwitter_room.html";
 }
